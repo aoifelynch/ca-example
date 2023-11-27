c756ed1fb98b2f93faf7687f5051cb89cdd7f222
Aoife Lynch <n00222385@iadt.ie>
2023-11-27 11:04:14 +0000

    Create Role Model and migrations

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\Role;
 
 class UserSeeder extends Seeder
 {
@@ -13,11 +14,25 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        $role_admin = Role::where('name', 'admin')->first();
+        $role_user = Role::where('name', 'user')->first();
+
         $admin = new User;
         $admin->name = "Aoife Lynch";
         $admin->email = "aoife@outlook.com";
         $admin->password = "louise108";
         $admin->save();
 
+        // attach admin role to the user created above
+        $admin->roles()->attach($role_admin);
+
+        $user = new User;
+        $user->name = "John Joe";
+        $user->email = "johnjoe@email.com";
+        $user->password = "password";
+        $user->save();
+
+        // attach admin role to the user created above
+        $user->roles()->attach($role_user);
     }
 }
